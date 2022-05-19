4f0e286ffcd85a0a757980a6ff4e38b0dbf6e8b7
Sungho Cho <didog9595@gmail.com>
2022-05-19 21:01:00 +0900

    Add mapbox and display a basic map in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Map, { Marker } from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGlkb2c5NSIsImEiOiJja3d1Ymd5N2swaGRjMm9xbjU1OGRkMDl0In0.jkmsYL4EYs3VmsU6zUxbPQ';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      initialViewState={{
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 14
+      }}
+      style={{ width: 800, height: 600 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" />
+    </Map>
   );
 }
 
