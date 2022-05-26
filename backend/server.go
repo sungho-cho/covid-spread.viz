47a71b6fe9a66ddad7dbe01ded386f594f990645
Sungho Cho <didog9595@gmail.com>
2022-05-26 20:53:52 +0900

    Write comments in server.go

--- backend/server.go
+++ backend/server.go
@@ -23,19 +23,20 @@ func (s *covidDataServer) GetActiveCases(ctx context.Context, req *pb.GetActiveC
 }
 
 func main() {
+	// Fetch covid data for all countries and save the data locally
 	go utils.Fetch()
 
+	// Set up a gRPC server
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
 	grpcServer := grpc.NewServer()
 	pb.RegisterCovidDataServer(grpcServer, &covidDataServer{})
 
+	// Start the gRPC server
 	log.Printf("start gRPC server on %s port", port)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
-	log.Println("end of main")
 }
