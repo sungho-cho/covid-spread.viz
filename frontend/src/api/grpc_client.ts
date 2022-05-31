cb473abb819cee4b4540f5b6522f0c1164a4285d
Sungho Cho <didog9595@gmail.com>
2022-05-31 18:56:27 +0900

    Update iso3 country codes to be repeated field in proto

--- frontend/src/api/grpc_client.ts
+++ frontend/src/api/grpc_client.ts
@@ -6,7 +6,7 @@ const gRPC_client = new CovidDataClient('http://localhost:8080');
 export function get_countries_data(date: Date, callback: (countries_data?: CountriesData) => void) {
   var proto_date = new ProtoDate();
   proto_date.setYear(date.getFullYear());
-  proto_date.setMonth(date.getMonth());
+  proto_date.setMonth(date.getMonth() + 1);
   proto_date.setDay(date.getDate());
   var request = new GetCountriesDataRequest();
   request.setDate(proto_date);
