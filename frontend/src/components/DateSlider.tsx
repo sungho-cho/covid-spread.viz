699d0a93196d02b3e6ef2259146687d2c9d3f444
Sungho Cho <didog9595@gmail.com>
2022-06-03 20:54:13 +0900

    Move DateSlider to another component

---
+++ frontend/src/components/DateSlider.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import Slider from '@mui/material/Slider'
+
+interface DateSliderProps {
+  date: Date,
+  setDate: (date: Date) => void
+  firstDate: Date,
+  lastDate: Date,
+}
+
+interface Mark {
+  value: number,
+  label: string,
+}
+
+const DateSlider = (props: DateSliderProps) => {
+  const { date, setDate, firstDate, lastDate } = props
+
+  const [marks, setMarks] = useState<Array<Mark>>([])
+
+  useEffect(() => {
+    let markMonth = new Date(Date.UTC(firstDate.getUTCFullYear(), firstDate.getUTCMonth()))
+    let lastMonth = new Date(Date.UTC(lastDate.getUTCFullYear(), lastDate.getUTCMonth()))
+    var newMarks = []
+    for (; markMonth.getTime() <= lastMonth.getTime();) {
+      const month = markMonth.getUTCMonth() + 1
+      var label = ""
+      if (month == 1) {
+        label = markMonth.getUTCFullYear().toString()
+      } else {
+        label = month.toString()
+      }
+      newMarks.push({ value: markMonth.getTime(), label: label })
+      markMonth = new Date(Date.UTC(markMonth.getUTCFullYear(), markMonth.getUTCMonth() + 1))
+    }
+    setMarks(newMarks)
+  }, [])
+
+  const valueLabelFormat = (dateNumber: number) => {
+    const date = new Date(dateNumber)
+    return date.toLocaleDateString("en-US");
+  }
+
+  const updateDate = (event: Event, newValue: number | number[]) => {
+    if (typeof newValue === 'number') {
+      setDate(new Date(newValue))
+    }
+  }
+
+  return (
+    <Slider
+      value={date.getTime()}
+      aria-label="Small steps"
+      defaultValue={firstDate.getTime()}
+      getAriaValueText={valueLabelFormat}
+      valueLabelFormat={valueLabelFormat}
+      step={86400000}
+      min={firstDate.getTime()}
+      max={lastDate.getTime()}
+      marks={marks}
+      valueLabelDisplay="auto"
+      onChange={updateDate}
+    />
+  )
+}
+
+export default DateSlider;
