6f8fe48b6e6e640282dadd7552bfee12d439e639
Sungho Cho <didog9595@gmail.com>
2022-06-07 20:23:06 +0900

    Add a few paragraphs to Info modal

--- frontend/src/components/Info.tsx
+++ frontend/src/components/Info.tsx
@@ -6,6 +6,7 @@ import InfoIcon from '@mui/icons-material/Info'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import ShareIcon from '@mui/icons-material/Share'
+import Link from '@mui/material/Link'
 import Modal from '@mui/material/Modal'
 import Popover from '@mui/material/Popover'
 import Typography from '@mui/material/Typography'
@@ -45,6 +46,8 @@ const Info = () => {
   const email = "email" // TODO: Change to real email address
   const githubURL = "https://github.com/sungho-cho"
   const linkedinURL = "https://linkedin.com/in/sungho-cho/"
+  const jhuURL = "https://github.com/CSSEGISandData/COVID-19"
+  const jhuRecoveryURL = "https://github.com/CSSEGISandData/COVID-19/issues/4465"
   const modalStyle = {
     position: 'absolute',
     top: '50%',
@@ -68,6 +71,9 @@ const Info = () => {
 
   return (
     <div className="info-button">
+      <Popover open={popoverOpen} anchorOrigin={anchorOrigin} transformOrigin={transformOrigin}>
+        <Typography sx={{ p: 1.5, fontSize: "14px" }}>{popoverText}</Typography>
+      </Popover>
       <IconButton aria-label="info" onClick={handleOpen}>
         <InfoIcon sx={{ fontSize: 30 }} />
       </IconButton>
@@ -83,12 +89,9 @@ const Info = () => {
             <IconButton aria-label="share" onClick={copyVizURL}>
               <ShareIcon sx={{ fontSize: iconSize }} />
             </IconButton>
-            <Popover open={popoverOpen} anchorOrigin={anchorOrigin} transformOrigin={transformOrigin}>
-              <Typography sx={{ p: 2 }}>{popoverText}</Typography>
-            </Popover>
           </Typography>
           <div className="info-modal-name">
-            <Typography variant="subtitle1" component="h3">
+            <Typography variant="subtitle1" component="h3" sx={{ fontWeight: 700 }}>
               Sungho Cho
             </Typography>
             <IconButton aria-label="github" onClick={() => openURL(githubURL)}>
@@ -104,9 +107,29 @@ const Info = () => {
           <Typography id="info-modal-career" variant="subtitle2" component="h4" gutterBottom>
             Carnegie Mellon '20 / Uber ATG / Aurora Innovation
           </Typography>
-          <Typography id="info-modal-description" variant="body2">
-            This is a description of this website
-          </Typography>
+          <div id="info-modal-description">
+            <Typography variant="body2">
+              This website's goal is to deliver fast visualization of historical data of COVID-19 since the outbreak to the present day.
+              It was built in order for us to better understand the tragic impact of the coronavirus pandemic over the world population.
+            </Typography>
+            <Typography variant="body2">
+              The data is from
+              <Link href={jhuURL} target="_blank" rel="noopener"> JHU CSSE COVID-19 Data</Link>.
+              It is updated daily when JHU CSSE publishes new data.
+              Note that recovery data for United States is absent since December 14, 2020,
+              as well as recovery data for all countries since August 5th, 2021,
+              as JHU CSSE stopped tracking recovery data
+              (<Link href={jhuRecoveryURL} target="_blank" rel="noopener">view details</Link>).
+            </Typography>
+            <Typography variant="body2">
+              This website was developed with React.js, GoLang, Mapbox, gRPC, and Envoy.
+              The web application as well as the back-end server are not open-sourced,
+              but you can checkout the GitHub repository to get a sense of the general project structure.
+            </Typography>
+            <Typography variant="body2">
+              Please feel free to email me with any feedback or questions :)
+            </Typography>
+          </div>
         </Box>
       </Modal>
     </div >
